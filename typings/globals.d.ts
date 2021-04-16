423fd559977b8192b3db3c4fab5b141b0dc552e2
zsuzitor <Avdosevstas@mail.ru>
2021-04-16 09:18:48 +0300

    typings

---
+++ typings/globals.d.ts
@@ -0,0 +1,25 @@
+// import * as AjaxHelper from "../src/components/_ComponentsLink/AjaxLogic";
+// import("../src/components/_ComponentsLink/AjaxLogic").IAjaxInputObject;
+
+
+
+declare var G_EmptyImagePath: string;
+declare var G_PathToBaseImages: string;
+declare var G_PathToServer: string;
+declare var G_PathToServerMenu: string;
+declare var G_PreloaderPath: string;
+declare var G_AjaxHelper: import("../src/components/_ComponentsLink/AjaxLogic").IAjaxHelper;//TODO записать
+declare var G_ErrorHandleLogic: import("../src/components/_ComponentsLink/Models/ErrorHandleLogic").IMainErrorHandler;
+declare var G_AddAbsoluteAlertToState: (alert: import("../src/components/_ComponentsLink/Models/AlertData").AlertData) => void;
+// declare var G_PathToServerMenu: string;
+// declare function GoAjaxRequest(obj:import("../src/components/_ComponentsLink/AjaxLogic").IAjaxInputObject, fileLoad:boolean=false):void;
+
+
+declare var G_AuthenticateController: import("../src/components/_ComponentsLink/Controllers/AuthenticateController").IAuthenticateController;
+declare var G_ArticleController: import("../src/components/_ComponentsLink/Controllers/MenuApp/ArticleController").IArticleController;
+declare var G_WordsCardsController: import("../src/components/_ComponentsLink/Controllers/WordsCardsApp/WordsCardsController").IWordsCardsController;
+declare var G_WordsListController: import("../src/components/_ComponentsLink/Controllers/WordsCardsApp/WordsListController").IWordsListController;
+declare var G_UsersController: import("../src/components/_ComponentsLink/Controllers/UsersController").IUsersController;
+
+
+
